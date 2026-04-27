1e8f831efdd94ed5a110f3c31918d81f9b79bf18
Maycolsilva2 <maykolestibensilva@gmail.com>
2026-04-27 12:45:48 -0500

    Se termino proyecto freelancer

---
+++ js/components/contact-form.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.services__form-container');
+
+  if (!form) return;
+
+  fetch('/views/components/contact-form.html')
+    // La respuesta la convierte a texto plano
+    .then((response) => response.text())
+    // Inserta el contenido html del navbar en el contenedor correspondiente.
+    .then((data) => {
+      form.innerHTML = data;
+    })
+    .catch((error) => console.error('Error cargando formulario', error));
+});
